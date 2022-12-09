bfcc2da65b398ca917216c2fdd4ce25ab38107d5
MICHELE LOTTO <875922@stud.unive.it>
2022-12-10 00:42:43 +0100

    Added X and y to csv Added grid search cv support for KNN Updated requirements Added GPU library for RF Added all SVM notebooks

--- KNN.py
+++ KNN.py
@@ -1,24 +1,29 @@
 from cmath import sqrt
 import numpy as np
 import statistics as st
-from tqdm.notebook import tqdm
+from tqdm.notebook import tqdm,tnrange
 
-from multiprocessing import Pool
 class KNN:
     
-    def __init__(self,k,n_jobs):
-        self.k=k
-        self.n_jobs=n_jobs        
+    def __init__(self,k=1):
+        self.k=k        
         
     def fit(self, X_data,y_data):
         self.X_train=X_data
         self.y_train=y_data
-    
-    def classify(self,point):
         
-        distances=np.array([])
-        for row in self.X_train:
-            distances=np.append(distances,np.linalg.norm(point-row))
+    def get_params(self,deep):
+        
+        return {'k':self.k}
+    
+    def set_params(self,**params):
+        
+        for key,value in params.items():
+            setattr(self, key, value)
+        return self
+    
+    def __classify(self,point):
+        distances=np.linalg.norm(self.X_train - point, axis=1)
         
         neighbors_y=np.array([])
         for _ in range(self.k):
@@ -32,24 +37,9 @@ class KNN:
         return st.mode(neighbors_y)
     
     def predict(self,data):
-        
-        pool=Pool()
-        results=[]
-        _from=0
-        _step=int(len(data)/self.n_jobs)
-        
-        while _from<len(data):
-            results+=[pool.apply_async(f,(self,data,_from,_from+_step,))]
-            _from+=_step
-        
-        for x in (results):
-            print(x.get(timeout=3600))
-
-def f(knn,data,_from,_to):
-    
         output_y=np.array([])
         
-        for x in range(_from,_to):
-            output_y=np.append(output_y,knn.classify(data[x]))
+        for row in data:
+            output_y=np.append(output_y,self.__classify(row))
     
-        return output_y
+        return output_y            
