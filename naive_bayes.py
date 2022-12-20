1b71cc032e79b595cd18ea3ad729bb6e5a08ce86
MICHELE LOTTO <875922@stud.unive.it>
2022-12-20 20:24:43 +0100

    Better bayes performance

--- naive_bayes.py
+++ naive_bayes.py
@@ -28,6 +28,10 @@ class BetaDistribution_NaiveBayes(BaseEstimator):
 
             unique_counts=images_class_n.nunique(axis=0, dropna=True)
             
+            unique_counts[unique_counts > 1] = -1
+            
+            unique_counts[unique_counts == 1] = means_pixels_class_n[unique_counts == 1]
+            
             ks_pixels_class_n=((means_pixels_class_n*(1-means_pixels_class_n))/variances_pixels_class_n)-1
             
             alphas_pixels_class_n=ks_pixels_class_n*means_pixels_class_n
@@ -37,15 +41,15 @@ class BetaDistribution_NaiveBayes(BaseEstimator):
                 
             self.parameter_per_class[n]={'alpha':alphas_pixels_class_n.to_numpy(),
                                         'beta':betas_pixels_class_n.to_numpy(),
-                                        'unique_counts':unique_counts,
+                                        'unique':unique_counts.to_numpy(),
                                         'mean':means_pixels_class_n,
                                         'var':variances_pixels_class_n,
                                         'k':ks_pixels_class_n,
                                         'frequency':frequency}
-            print(self.parameter_per_class)
-            
+            #print(self.parameter_per_class)
+    
     def predict(self,test_X:pd.DataFrame):
-        
+    
         epsilon=0.1
         
         output=[]
@@ -56,47 +60,32 @@ class BetaDistribution_NaiveBayes(BaseEstimator):
             _max=0
             _max_class=-1
             
+            row=row.to_numpy()
+             
             for n in range(10):
                 
                 class_parameters=self.parameter_per_class[n]
                 
-                product=1
+                _alpha=class_parameters['alpha']
+                _beta=class_parameters['beta']
+                _unique=class_parameters['unique']
                 
-                for i,x in enumerate(row):
-                    
-                    if class_parameters['unique_counts'][i]!=1:
+                
+                beta_probabilities=beta.cdf(row+epsilon,_alpha,_beta)-beta.cdf(row-epsilon,_alpha,_beta)               
+                
+                #unique values
+                
+                beta_probabilities[ np.logical_and(_unique!=-1, _unique != row) ] = 0
+                beta_probabilities[ np.logical_and(_unique!=-1, _unique == row) ] = 1
+                
+                #alpha and beta < 0
+                
+                #print("alpha",_alpha[ np.argwhere( np.isnan(beta_probabilities) ) ])
+                #print("beta",_beta[ np.argwhere( np.isnan(beta_probabilities) ) ])
                         
-                        _alpha=class_parameters['alpha'][i]
-                        _beta=class_parameters['beta'][i]
-                        
-                        _mean=class_parameters['mean'][i]
-                        _var=class_parameters['var'][i]
-                        _k=class_parameters['k'][i]
-                        
-                            
-                        prob=beta.cdf(x+epsilon,_alpha,_beta)-beta.cdf(x-epsilon,_alpha,_beta)
-                    
-                        if np.isnan(prob) or prob==float("inf") or prob==float("-inf"):
-                            """
-                            print(prob)
-                            print(n,i)
-                            print(class_parameters['unique_counts'][i])
-                            print(x,_alpha,_beta,_mean,_var,_k) 
-                            """
-                            pass
-                        else:
-                            #print(prob) 
-                            product*=prob 
-                    else:
-                        if x==class_parameters['mean'][i]:
-                            product*=1
-                        else:
-                            product*=0
-                        
-                    if product==0:
-                        break
-                    
-                probability=product*class_parameters['frequency']
+                np.nan_to_num(beta_probabilities, copy=False, nan=1.0)
+                
+                probability=class_parameters['frequency']*np.product(beta_probabilities)
                 
                 if probability>_max:
                     _max=probability
@@ -105,54 +94,6 @@ class BetaDistribution_NaiveBayes(BaseEstimator):
             
             output.append(_max_class)
             indexes.append(row_i)
-        
-        return pd.Series(data=output,index=indexes) 
-     
-    """
-    def __param_estimation(self):
-        
-        mean=self.train_X.mean(axis=0)
-        var=self.train_X.var(axis=0)
-        
-        k=( ( mean*( 1-mean ) ) / var )-1
-        
-        self.alpha=k*mean
-        self.beta=k*(1-mean)
-        
-        self.integrals_pixels=[]
             
-        for item in zip(self.alpha, self.beta):
-            self.integrals_pixels.append(quad(BetaDistribution_NaiveBayes.__B,0,1,args=(item[0],item[1]))[0])
-        
-        self.frequencies=[]
-        
-        for n in range(10):
-            self.frequencies.append(self.train_y[self.train_y["class"]==n].size/self.train_y["class"].size)
-    
-    def predict(self,test_X:pd.DataFrame):
-        
-        output=[]
-        
-        for _,row in test_X.iterrows():
-                
-            product=1
-            
-            for i,x in enumerate(row):
-    
-                try:
-                    product*=self.integrals_pixels[i]*math.pow(x,self.alpha[i]-1)*math.pow(1-x,self.beta[i]-1)
-                except:
-                    product*=1
-            
-                _max=0
-                _index=None
-                for n in range(10):
-                    if _max<(product*self.frequencies[n]):
-                        _max=(product*self.frequencies[n])
-                        _index=n
-                
-            output.append(_index)
-            
-        return output                   
-    """
+        return pd.Series(data=output,index=indexes)
         
